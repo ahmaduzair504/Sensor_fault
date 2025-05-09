c56dd59cf088fc751f8d1f882ba9fe9a2b477fc5
Ahmad Uzair <ahmaduzair2585@gmail.com>
2025-05-09 19:50:03 +0500

    updated README and config

--- setup.py
+++ setup.py
@@ -2,11 +2,17 @@ from setuptools import find_packages, setup
 # This is the setup file for the Sensor fault detection project.
 from typing import List
 
+def get_requirements()-> List[str]:
+    requirements_list =list[str] = []
+    return requirements_list
+
+
 setup(
     name="Sensor fault detection",
     version="0.0.1",
     author="Ahmad Uzair",
     author_email="ahmaduzair2585@gmail.com",
     packages= find_packages(),
-    install_requires=["pymongo"]
+    install_requires= get_requirements(),
+    
 )
